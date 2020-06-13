bd5bcc88de05cd14d1332f1e258add5ab7e76f8a
rusk <lordruskk@gmail.com>
2020-06-13 10:45:50 -0700

    changed default terminal size again

--- config.h
+++ config.h
@@ -162,7 +162,7 @@ static unsigned int cursorshape = 2;
  * Default columns and rows numbers
  */
 
-static unsigned int cols = 110;
+static unsigned int cols = 140;
 static unsigned int rows = 34;
 
 /*
